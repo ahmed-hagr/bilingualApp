9ed76231fa65201925f713d7f77b3f838fcf93c7
ahmed-hagr <46416385+ahmed-hagr@users.noreply.github.com>
2024-08-17 23:35:04 +0300

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   mode: "jit", // JIT mode enabled
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
